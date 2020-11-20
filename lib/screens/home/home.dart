a7f69b683b9f9ace5f7c2e317742be1519e7d1a8
unknown <joao.basto.rosario@gmail.com>
2020-11-20 15:51:56 +0000

    added listeners and streams to sign in and sign out

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/services/auth.dart';
 
 class Home extends StatelessWidget {
+
+  final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('home'),
-    );
+    return Scaffold(
+      backgroundColor: Colors.green[50],
+      appBar: AppBar(
+        title: Text('Coffe Paste'),
+        backgroundColor: Colors.green[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('logout'),
+            onPressed: () async{
+              await _auth.signOut();
+            },
+          )
+        ],
+      ),
+      );
   }
 }
