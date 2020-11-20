2ccc86135dc92467cc231a8a3ac328a57fd2cd6a
unknown <joao.basto.rosario@gmail.com>
2020-11-20 16:02:33 +0000

    added home option for call

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/jitsii/session.dart';
 import 'package:flutter_app/services/auth.dart';
 
 class Home extends StatelessWidget {
@@ -9,7 +10,7 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.green[50],
       appBar: AppBar(
-        title: Text('Coffe Paste'),
+        title: Text('Clip Meet'),
         backgroundColor: Colors.green[400],
         elevation: 0.0,
         actions: <Widget>[
@@ -22,6 +23,36 @@ class Home extends StatelessWidget {
           )
         ],
       ),
-      );
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+        SizedBox(height: 40.0),
+        Container(
+          height: 40.0,
+            child: Material(
+              borderRadius: BorderRadius.circular(20.0),
+                shadowColor: Colors.greenAccent,
+                color: Colors.green,
+                elevation: 7.0,
+                child: GestureDetector(
+                  onTap: () {
+                  Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CreateSession()));
+    },
+                  child: Center(
+                    child: Text(
+                      'Create Session',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat'),
+                    ),
+                  ),
+                )
+            )
+        )
+        ]),
+    );
   }
 }
